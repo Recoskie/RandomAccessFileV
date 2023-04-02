eca69feae6752caeccb731355d243466b926f345
Damian Recoskie <damian_programmer@hotmail.com>
2023-04-02 12:20:08 -0400

    Update

--- FileReaderV.js
+++ FileReaderV.js
@@ -515,7 +515,7 @@ FileReaderV.prototype.initBufV = function()
 
 //This is a function that waits till all data processing is finished before calling a method.
 
-FileReaderV.prototype.wait = function(func) { if( this.Events ) { func(); return; } setTimeout(function(func,r){r.wait(func);},0,func,this); }
+FileReaderV.prototype.wait = function(func) { if( this.Events ) { func(); return(false); } setTimeout(function(func,r){r.wait(func);},0,func,this); return(true); }
         
 FileReaderV.prototype.fr.onerror = FileReaderV.prototype.frv.onerror = function() { console.error("File IO Error!"); }
 
